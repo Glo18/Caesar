e59648ba482d16ec9357802fbc05b26348b8552c
Glo18 <kiogoraspecial@gmail.com>
2021-02-15 12:23:12 +0300

    import console

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import models.Cipher;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+    }
+}
