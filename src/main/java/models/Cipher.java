c6663745c0efc58a4b6aee6b1c48b6d2b524f892
Glo18 <kiogoraspecial@gmail.com>
2021-02-15 11:51:12 +0300

    create constructor and test passes

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Cipher {
+    private String mStatement;
+    private int mKey;
+
+    public Cipher(String Statement, int Key) {
+        mStatement = Statement;
+        mKey = Key;
+    }
 }
