4173c1d0595398f7f1139d7017a9d65b49de3295
Glo18 <kiogoraspecial@gmail.com>
2021-02-15 12:26:30 +0300

    enter prompts for user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,5 +6,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Console myConsole = System.console();
-    }
+
+        boolean programRunning = true;
+
+        while (programRunning) {
+            System.out.println("Welcome! Would you like to encrypt or decrypt a statement?");
+            String userEncrypt = myConsole.readLine();
+
+            if (userEncrypt.equals("encrypt")) {
+                System.out.println("Enter your statement:");
+                    String statement = myConsole.readLine();
+                System.out.println("Enter encryption key:");
+                int key = myScanner.nextInt();
+
+                Cipher userEncryption = new Cipher(statement, key);
+                String encryption = userEncryption.isEncrypted(statement, key);
+
+                System.out.println("Your encrypted statement is: \n" + encryption);
+            }
+        }
 }
