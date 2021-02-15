7b01d36aaee6fa34de40f7274e193db276a32a31
Glo18 <kiogoraspecial@gmail.com>
2021-02-15 12:03:44 +0300

    encryption test fails the pass

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -18,4 +18,22 @@ public class Cipher {
         mStatement = "b";
         return mStatement;
     }
+    public String isEncrypted(String statement, int key) {
+        String results = "";
+        StringBuffer encrypted = new StringBuffer();
+
+        for (int i = 0; i < statement.length(); i++) {
+            int encrypt = statement.charAt(i);
+            if (encrypt + key > 122) {
+                char encrypts = (char) (encrypt - 26 + key);
+                encrypted.append(encrypts);
+            }
+            else {
+                char encrypts = (char) (encrypt + (key % 26));
+                encrypted.append(encrypts);
+            }
+        }
+        results +=encrypted;
+        return results;
+    }
 }
