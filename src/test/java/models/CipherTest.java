34875cfb12dda5b9de9ade5d4f7e5a3c7a5459ed
Glo18 <kiogoraspecial@gmail.com>
2021-02-15 11:47:54 +0300

    create test then fail

--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -1,7 +1,12 @@
 package models;
 
-import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
 
-public class CipherTest extends TestCase {
-
+public class CipherTest {
+    @Test
+    public void newCipher_instantiatesCorrectly() {
+        Cipher testCipher = new Cipher("a", 2);
+        assertEquals(true, testCipher instanceof Cipher);
+    }
 }
