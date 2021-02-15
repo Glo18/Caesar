7798b768e09e0a15e0ceb93ab6435ddd85427485
Glo18 <kiogoraspecial@gmail.com>
2021-02-15 12:39:40 +0300

    parse user's input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,20 +4,25 @@ import java.io.Console;
 import java.util.Scanner;
 
 public class App {
+
+
     public static void main(String[] args) {
         Console myConsole = System.console();
+//        Scanner myScanner = new Scanner(System.in);
 
         boolean programRunning = true;
 
         while (programRunning) {
             System.out.println("Welcome! Would you like to encrypt or decrypt a statement?");
             String userEncrypt = myConsole.readLine();
+//            String userEncrypt = myScanner.nextLine();
 
             if (userEncrypt.equals("encrypt")) {
                 System.out.println("Enter your statement:");
-                String statement = myConsole.readLine();
+                    String statement = myConsole.readLine();
+//                String statement = myScanner.nextLine();
                 System.out.println("Enter encryption key:");
-                int key = myScanner.nextInt();
+                int key = Integer.parseInt(myConsole.readLine());
 
                 Cipher userEncryption = new Cipher(statement, key);
                 String encryption = userEncryption.isEncrypted(statement, key);
@@ -26,9 +31,10 @@ public class App {
             } else if (userEncrypt.equals("decrypt")) {
                 System.out.println("Enter your statement:");
                     String statementDecrypt = myConsole.readLine();
+//                String statementDecrypt = myScanner.nextLine();
 
                 System.out.println("Enter decryption key:");
-                int keyDecrypt = myScanner.nextInt();
+                int keyDecrypt = Integer.parseInt(myConsole.readLine());
 
                 Cipher userDecryption = new Cipher(statementDecrypt, keyDecrypt);
                 String decryption = userDecryption.toDecrypt(statementDecrypt, keyDecrypt);
@@ -41,5 +47,6 @@ public class App {
 
             }
         }
+
     }
 }
